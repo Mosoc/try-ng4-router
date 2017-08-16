468a11d782a3378bdbf547abdc55c1aa093e092e
Pojen Chen <mosochin@tagtoo.org>
2017-08-16 19:07:50 +0900

    Set nested route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent }   from './pages/index/index.component';
 import { FirstComponent }   from './pages/first/first.component';
 import { UserComponent} from './pages/user/user.component'
+import { NestComponent } from './pages/nest/nest.component';
+import { EggComponent } from './pages/nest/egg/egg.component';
+import { BirdComponent } from './pages/nest/bird/bird.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index',  component: IndexComponent },
   { path: 'first',  component: FirstComponent },
   { path: 'user/:userId',  component: UserComponent },
+  {
+    path: 'nest',
+    component: NestComponent,
+    children: [
+      {path: 'egg', component: EggComponent}, 
+      {path: 'bird', component: BirdComponent}, 
+    ]
+  }
 ];
 
 @NgModule({
