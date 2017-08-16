8cb1b13b2c110a5a762ec4d32a5e01961896e8c5
Pojen Chen <mosochin@tagtoo.org>
2017-08-16 11:51:26 +0900

    Set routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent }   from './pages/index/index.component';
+import { FirstComponent }   from './pages/first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index',  component: IndexComponent },
+  { path: 'first',  component: FirstComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
